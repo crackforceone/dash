b8e325161eefdb95ddd9b48f438df4c90d4edc90
crackforceone <wlados45@web.de>
2026-02-09 20:30:45 +0000

    install script qt-gstremer repo update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #repo addresses
 aasdkRepo="https://github.com/OpenDsh/aasdk"
-gstreamerRepo="https://github.com/GStreamer/qt-gstreamer"
+gstreamerRepo="https://github.com/crackforceone/qt-gstreamer"
 openautoRepo="https://github.com/crackforceone/openauto"
 h264bitstreamRepo="https://github.com/aizvorski/h264bitstream"
 pulseaudioRepo="https://gitlab.freedesktop.org/pulseaudio/pulseaudio.git"
